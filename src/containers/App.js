863250a84d013a9a2fd5dbbc506e5fe6072d3e50
Bryan Le <bryan@bryanle.io>
2018-01-02 23:07:55 -0500

    feat: api request to cryptocurrency api

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      prices: {}
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,IOTA,XRP,LTC&tsyms=BTC,USD,ETH,LTC')
+      .then((res) => this.setState({ prices: res.data }))
+  }
+
+  printPrices(cryptoType) {
+    console.log(cryptoType)
+  }
+
   render() {
+
+    console.log(this.state.prices);
     return (
       <div className="App">
         Hello World
