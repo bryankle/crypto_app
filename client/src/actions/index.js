ecc0bbc401bb97a63a3eef9cff7ffb4f751136c4
Bryan Le <bryan@bryanle.io>
2018-02-02 00:31:09 -0500

    update

---
+++ client/src/actions/index.js
@@ -0,0 +1,8 @@
+export function signinUser({ email, password }) {
+    // Submit email/password to the server
+
+    // If request is good
+        // Update state to indicate suer is authenticated
+        // Save the JWT token - to make followup requests in the backend
+        // redirect to the route /feature
+}
