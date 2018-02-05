644cea6a009309239fd391a81a75903d0469d796
Bryan Le <bryan@bryanle.io>
2018-02-04 21:39:22 -0500

    fix: changed database url

--- database/models/db.js
+++ database/models/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize")
-const db = new Sequelize("postgres://localhost:5432/crypto_app", {
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/crypto_app", {
     logging: false
 })
 
