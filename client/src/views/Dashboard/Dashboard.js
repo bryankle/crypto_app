18f37180f83fdd5c51a1455f7623617e325089ba
Bryan Le <bryan@bryanle.io>
2018-03-26 01:17:45 -0400

    feat: top 50 coins rendered in separate rows - next fix for column

--- client/src/views/Dashboard/Dashboard.js
+++ client/src/views/Dashboard/Dashboard.js
@@ -18,162 +18,169 @@ import LTC from '../../assets/images/ltc.png';
 
 import { Content, Navbar, Main, CryptoIcon } from './style';
 
-
 class Dashboard extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            prices: {},
-            topCoins: {}
+            images: {},
+            prices: {}
         }
     }
 
+    componentWillMount() {
+        const images = this.importAll(require.context('../../assets/images/logos', false, /\.(png|jpe?g|svg)$/));
+        this.setState({ images })
+    }
+
     componentDidMount() {
+
         axios
-            .get(
-                'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD'
-            )
+            .get('https://api.coinmarketcap.com/v1/ticker/?limit=50')
             .then(res => this.setState({ prices: res.data }))
     }
 
-    renderTopCoins() {
-        
+    buildQuery = () => {
+        console.log('Building query')
+        const fileName = /[^\\\/]+(?=\.[\w]+$)|[^\\\/]+$/g;
+        const coins = Object.keys(this.state.images).map(function (val) {
+            return val.match(fileName)[0];
+        }).join(',')
+        return `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${coins}&tsyms=USD`
+    }
+
+    importAll(r) {
+        let images = {};
+        r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+        return images;
+    }
+
+
+    renderTopCoins = () => {
+        console.log('this.state')
+        console.log(this.state.prices)
+        let counter = 0;
+        if (counter == 3) counter = 0;
+        let mainElement = [];
+        let column = [];
+        let row = <Grid.Row columns={4}>
+            {[...column]}
+        </Grid.Row>
+
+        for (let i = 0; i < 50; i++) {
+            if (counter % 4 === 0) {
+            
+                counter = 0;
+                mainElement.push(row);
+                row = <Grid.Row columns={4}>
+                    {[...column]}
+            </Grid.Row>;
+            column = [];
+            }
+            // const [name, price_usd]  = this.state.prices[i];
+            column.push(<Grid.Column>
+                <Segment>
+                    <div>
+                        <CryptoIcon src={this.state.images[`${this.state.prices[i].symbol}.png`]} />
+                        <span style={{ textAlign: 'right' }}>
+                            <p>{this.state.prices[i].name}</p>
+                            <h3>${this.state.prices[i].price_usd}</h3>
+                        </span>
+                    </div>
+                    <Divider />
+                    <p style={{ color: '#a9a9a9', fontSize: '12px' }}>Updated 10 minutes ago</p>
+                </Segment>
+            </Grid.Column>)
+        }
+        console.log('Main element');
+        console.log(mainElement)
+        return mainElement;
     }
 
     render() {
-        console.log(this.state.prices)
-        console.log(Object.keys(this.state.prices))
+        // console.log(this.state)
+        // console.log(Object.keys(this.state.prices))
         if (Object.keys(this.state.prices).length > 0) {
             return (
                 <div>
-                <Sidebar />
+                    <Sidebar />
 
-                <Main>
+                    <Main>
 
-                    <Navbar />
+                        <Navbar />
 
-                    <Content>
-                        <Grid padded stackable>
-                            <Grid.Row columns={4}>
-                                <Grid.Column>
-                                    <Segment>
-                                        <div>
-                                            <Icon style={{ float: 'left' }} size='huge' name='bitcoin' color='yellow' />
-                                            <span style={{ textAlign: 'right' }}>
-                                                <p>Total Balance</p>
-                                                <h3>$1,234</h3>
-                                            </span>
-                                        </div>
-                                        <Divider />
-                                        <p style={{ color: '#a9a9a9', fontSize: '12px' }}>Updated just now</p>
-                                    </Segment>
-                                </Grid.Column>
-                                {/* <Grid.Column>
-                                    <Segment>
-                                        <div>
-                                            <Icon style={{ float: 'left' }} size='huge' name='percent' color='green' />
-                                            <span style={{ textAlign: 'right' }}>
-                                                <p>Change</p>
-                                                <h3>34.12</h3>
-                                            </span>
-                                        </div>
-                                        <Divider />
-                                        <p style={{ color: '#a9a9a9', fontSize: '12px' }}>Updated 10 minutes ago</p>
-                                    </Segment>
-                                </Grid.Column> */}
-                                <Grid.Column>
-                                    <Segment>
-                                        <div>
-                                            <CryptoIcon src={BTC} />
-                                            <span style={{ textAlign: 'right' }}>
-                                                <p>Bitcoin</p>
-                                                <h3>${this.state.prices['BTC']['USD']}</h3>
-                                            </span>
-                                        </div>
-                                        <Divider />
-                                        <p style={{ color: '#a9a9a9', fontSize: '12px' }}>Updated 10 minutes ago</p>
-                                    </Segment>
-                                </Grid.Column>
-                                <Grid.Column>
-                                    <Segment>
-                                        <div>
-                                            <CryptoIcon src={ETH} />
-                                            <span style={{ textAlign: 'right' }}>
-                                                <p>Ethereum</p>
-                                                <h3>${this.state.prices['ETH']['USD']}</h3>
-                                            </span>
-                                        </div>
-                                        <Divider />
-                                        <p style={{ color: '#a9a9a9', fontSize: '12px' }}>Updated 10 minutes ago</p>
-                                    </Segment>
-                                </Grid.Column>
+                        <Content>
+                            <Grid padded stackable>
+                            {this.renderTopCoins()}
+                                {/* <Grid.Row columns={4}>
 
-                                <Grid.Column>
-                                    <Segment>
-                                        <div>
-                                            <CryptoIcon src={LTC} />
-                                            <span style={{ textAlign: 'right' }}>
-                                                <p>Litecoin</p>
-                                                <h3>${this.state.prices['LTC']['USD']}</h3>
-                                            </span>
-                                        </div>
-                                        <Divider />
-                                        <p style={{ color: '#a9a9a9', fontSize: '12px' }}>Updated 10 minutes ago</p>
-                                    </Segment>
-                                </Grid.Column>
-                            </Grid.Row>
+                                    <Grid.Column>
+                                        <Segment>
+                                            <div>
+                                                <CryptoIcon src={this.state.images['BTC.png']} />
+                                                <span style={{ textAlign: 'right' }}>
+                                                    <p>{this.state.prices[0].name}</p>
+                                                    <h3>${this.state.prices[0].price_usd}</h3>
+                                                </span>
+                                            </div>
+                                            <Divider />
+                                            <p style={{ color: '#a9a9a9', fontSize: '12px' }}>Updated 10 minutes ago</p>
+                                        </Segment>
+                                    </Grid.Column>
 
 
-                            <Grid.Row columns={2}>
-                                <Grid.Column>
-                                    <Segment>
-                                        <div>
-                                            <Icon style={{ float: 'left' }} size='huge' name='bitcoin' color='yellow' />
-                                            <span style={{ textAlign: 'right' }}>
-                                                <p>Total Balance</p>
-                                                <h3>$1,234</h3>
-                                            </span>
-                                        </div>
-                                        <Divider />
-                                        <p style={{ color: '#a9a9a9', fontSize: '12px' }}>Updated just now</p>
-                                    </Segment>
-                                </Grid.Column>
-                                <Grid.Column>
-                                    <Segment>
-                                        <div>
-                                            <Icon style={{ float: 'left' }} size='huge' name='bitcoin' color='yellow' />
-                                            <span style={{ textAlign: 'right' }}>
-                                                <p>Total Balance</p>
-                                                <h3>$1,234</h3>
-                                            </span>
-                                        </div>
-                                        <Divider />
-                                        <p style={{ color: '#a9a9a9', fontSize: '12px' }}>Updated 10 minutes ago</p>
-                                    </Segment>
-                                </Grid.Column>
+                                </Grid.Row> */}
 
-                            </Grid.Row>
 
-                            <Grid.Row columns={1}>
-                                <Grid.Column>
-                                    <Segment>
-                                        <div>
-                                            <Icon style={{ float: 'left' }} size='huge' name='bitcoin' color='yellow' />
-                                            <span style={{ textAlign: 'right' }}>
-                                                <p>Total Balance</p>
-                                                <h3>$1,234</h3>
-                                            </span>
-                                        </div>
-                                        <Divider />
-                                        <p style={{ color: '#a9a9a9', fontSize: '12px' }}>Updated just now</p>
-                                    </Segment>
-                                </Grid.Column>
-                            </Grid.Row>
+                                <Grid.Row columns={2}>
+                                    <Grid.Column>
+                                        <Segment>
+                                            <div>
+                                                <Icon style={{ float: 'left' }} size='huge' name='bitcoin' color='yellow' />
+                                                <span style={{ textAlign: 'right' }}>
+                                                    <p>Total Balance</p>
+                                                    <h3>$1,234</h3>
+                                                </span>
+                                            </div>
+                                            <Divider />
+                                            <p style={{ color: '#a9a9a9', fontSize: '12px' }}>Updated just now</p>
+                                        </Segment>
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        <Segment>
+                                            <div>
+                                                <Icon style={{ float: 'left' }} size='huge' name='bitcoin' color='yellow' />
+                                                <span style={{ textAlign: 'right' }}>
+                                                    <p>Total Balance</p>
+                                                    <h3>$1,234</h3>
+                                                </span>
+                                            </div>
+                                            <Divider />
+                                            <p style={{ color: '#a9a9a9', fontSize: '12px' }}>Updated 10 minutes ago</p>
+                                        </Segment>
+                                    </Grid.Column>
 
-                        </Grid>
-                    </Content>
-                </Main>
-            </div>
+                                </Grid.Row>
+
+                                <Grid.Row columns={1}>
+                                    <Grid.Column>
+                                        <Segment>
+                                            <div>
+                                                <Icon style={{ float: 'left' }} size='huge' name='bitcoin' color='yellow' />
+                                                <span style={{ textAlign: 'right' }}>
+                                                    <p>Total Balance</p>
+                                                    <h3>$1,234</h3>
+                                                </span>
+                                            </div>
+                                            <Divider />
+                                            <p style={{ color: '#a9a9a9', fontSize: '12px' }}>Updated just now</p>
+                                        </Segment>
+                                    </Grid.Column>
+                                </Grid.Row>
+
+                            </Grid>
+                        </Content>
+                    </Main>
+                </div>
             )
 
         }
--- client/src/views/Dashboard/style.js
+++ client/src/views/Dashboard/style.js
@@ -2,64 +2,6 @@ import styled, { css } from 'styled-components';
 import SidebarBackground from '../../assets/images/wave.jpg';
 import UserImage from '../../assets/images/face.jpg';
 
-export const Sidebar = styled.div`
-    height: 100%; /* Full-height: remove this if you want "auto" height */
-    width: 260px; /* Set the width of the sidebar */
-    position: fixed; /* Fixed Sidebar (stay in place on scroll) */
-    z-index: 1; /* Stay on top */
-    top: 0; /* Stay at the top */
-    left: 0;
-    overflow-x: hidden; /* Disable horizontal scroll */
-    padding-top: 20px;
-    background-image: url(${SidebarBackground}), linear-gradient(rgba(0,0,0,0.9),rgba(0,0,0,0.5));
-    background-blend-mode: overlay;
-`
-
-export const SidebarListItem = styled.li`
-    list-style-type: none;
-    width: 50px;
-    margin-top: 5px;
-    padding-bottom: 5px;
-    margin-left: 15px;
-    border-radius: 5px;
-    width: 220px;
-    height: 40px;
-    cursor: pointer;
-    &:hover ${SidebarListItem} {
-        transition: background-color 0.3s ease;
-        background-color: rgba(255,255,255,0.3);
-    }
-    ${props => props.user && css`
-        padding-left: 10px;
-        color: white;
-    `}
-`
-
-export const SidebarListLink = styled.a`
-    position: relative;
-    margin-left: 5%;
-    top: 25%;
-`
-
-export const SidebarText = styled.p`
-    color: #fff;
-    font-size: 12px;
-    font-weight: 600;
-    text-transform: uppercase;
-    display: inline;
-    padding-left: 15px;
-    ${props => props.user && css`
-        bottom: 7px;
-        position: relative;
-        font-weight: 100
-    `}
-`
-
-export const SidebarWrapper = styled.ul`
-    padding-left: 0;
-    margin: 0;
-`
-
 export const User = styled.div`
     display: inline-block;
     width: 30px;
